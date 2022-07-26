f7a4ef356efe85b4c4e35156bf80ad12228b9a0a
Ricards Tagils <tagils@mail.ru>
2022-07-26 23:52:20 +0400

    chore: add preline ui library

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,56 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div class="container mx-auto">
+      <div class="relative rounded-xl overflow-auto p-8">
+        <div class="grid grid-cols-3 gap-4 font-mono text-white text-sm text-center font-bold">
+          <div class="p-4 rounded-lg bg-indigo-300 dark:bg-indigo-800 dark:text-indigo-400">
+            01
+          </div>
+          <div class="p-4 rounded-lg bg-indigo-300 dark:bg-indigo-800 dark:text-indigo-400">
+            02
+          </div>
+          <div class="p-4 rounded-lg bg-indigo-300 dark:bg-indigo-800 dark:text-indigo-400">
+            03
+          </div>
+          <div class="p-4 rounded-lg shadow-lg bg-indigo-500 col-span-2">
+            04
+          </div>
+          <div class="p-4 rounded-lg bg-indigo-300 dark:bg-indigo-800 dark:text-indigo-400">
+            05
+          </div>
+          <div class="p-4 rounded-lg bg-indigo-300 dark:bg-indigo-800 dark:text-indigo-400">
+            06
+          </div>
+          <div class="p-4 rounded-lg shadow-lg bg-indigo-500 col-span-2">
+            07
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <button
+        type="button"
+        class="hs-collapse-toggle py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800"
+        id="hs-basic-collapse"
+        data-hs-collapse="#hs-basic-collapse-heading"
+      >
+        Collapse
+      </button>
+      <div
+        id="hs-basic-collapse-heading"
+        class="hs-collapse hidden w-full overflow-hidden transition-[height] duration-300"
+        aria-labelledby="hs-basic-collapse"
+      >
+        <div class="mt-5">
+          <p class="text-gray-500 dark:text-gray-400">
+            This is a collapse body. It is hidden by default, until the collapse
+            plugin adds the appropriate classes that we use to style each
+            element. These classes control the overall appearance, as well as
+            the showing and hiding via CSS transitions.
+          </p>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   );
-}
+};
 
 export default App;
