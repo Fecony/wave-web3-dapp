1a64308bab7760dc16c92e7732a5115d04dc3f36
Ricards Tagils <tagils@mail.ru>
2022-07-26 23:54:52 +0400

    chore: add ethers library

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,29 +26,6 @@ const App = () => {
           </div>
         </div>
       </div>
-
-      <button
-        type="button"
-        class="hs-collapse-toggle py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800"
-        id="hs-basic-collapse"
-        data-hs-collapse="#hs-basic-collapse-heading"
-      >
-        Collapse
-      </button>
-      <div
-        id="hs-basic-collapse-heading"
-        class="hs-collapse hidden w-full overflow-hidden transition-[height] duration-300"
-        aria-labelledby="hs-basic-collapse"
-      >
-        <div class="mt-5">
-          <p class="text-gray-500 dark:text-gray-400">
-            This is a collapse body. It is hidden by default, until the collapse
-            plugin adds the appropriate classes that we use to style each
-            element. These classes control the overall appearance, as well as
-            the showing and hiding via CSS transitions.
-          </p>
-        </div>
-      </div>
     </div>
   );
 };
